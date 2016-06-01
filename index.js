ed47d7d7beb7b913e8aa86bfcd420d1e39604802
wuct <wu.chingting@gmail.com>
2016-06-01 23:16:45 +0800

    Lint

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-module.exports = (compiler, renderer, options) => {
+const middlewareCreator = (compiler, renderer, options) => {
   // the state, false: bundle invalid, true: bundle valid
-  var state = false
-  var callbacks = []
+  let state = false
+  let queue = []
 
-  var filename =
+  const watchOptions = {
+    aggregateTimeout: 200,
+    ...options.watchOptions,
+  }
+  
+  const filename =
     `${compiler.options.output.path}/${compiler.options.output.filename}`
 
   compiler.plugin('done', (stats) => {
-    if (!options.quiet) console.log(stats.toString({ color: true }))
+    if (!options.quiet) console.log(stats.toString())
 
     state = true
 
@@ -17,25 +22,24 @@ module.exports = (compiler, renderer, options) => {
     // if a change happend while compiling
     process.nextTick(() => {
       // check if still in valid state
-      if(!state) returnw
+      if(!state) return
 
       console.info('webpack: bundle is now VALID.')
 
-      // execute callback that are delayed
-      var cbs = callbacks
-      callbacks = []
-      cbs.forEach(cb => cb())
+      // execute callbacks that are delayed
+      queue.forEach(cb => cb())
+      queue = []
     })
   })
 
-  function invalidPlugin() {
+  const invalidPlugin = () => {
     if (state) {
       console.info('webpack: bundle is now INVALID.')
     }
     state = false
   }
 
-  function invalidAsyncPlugin(_, callback) {
+  const invalidAsyncPlugin = (_, callback) => {
     invalidPlugin()
     callback()
   }
@@ -44,27 +48,25 @@ module.exports = (compiler, renderer, options) => {
   compiler.plugin('watch-run', invalidAsyncPlugin)
   compiler.plugin('run', invalidAsyncPlugin)
 
-
   // wait for bundle valid
-  function ready(fn, req) {
+  const ready = (fn, req) => {
     if (state) return fn()
     console.log(`webpack: wait until bundle finished: ${req.url || fn.name}`)
-    callbacks.push(fn)
+    queue.push(fn)
   }
 
-  compiler.watch(options.watchOptions, err => {
+  compiler.watch(watchOptions, err => {
     if(err) throw err
   })
 
-  function webpackDevMiddleware(req, res, next) {
-
+  return (req, res, next) => {
     // delay the request until we have a vaild bundle
     ready(processRequest, req)
 
     function processRequest() {
-      res.send('<!doctype html>\n' + renderer(require(filename)))
+      res.send('<!doctype html>\n' + renderer(require(filename), req))
     }
   }
-
-  return webpackDevMiddleware
 }
+
+export default middlewareCreator
